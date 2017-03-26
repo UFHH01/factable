13c50732bd6c5f37cd9d842546f11a80766318a5
Wes Jefferson Ong <wesjong@gmail.com>
2017-03-26 03:02:05 -0400

    Adds newspaper link and multi-sentence processing

--- factcheck/actualtest.py
+++ factcheck/actualtest.py
@@ -1,9 +1,47 @@
+import requests
+from bs4 import BeautifulSoup
+from django.core.exceptions import ValidationError
+from django.forms import URLField
+from newspaper import Article
+
 import factcheck
 
+
+def validate_url(url):
+    url_form_field = URLField()
+    try:
+        url = url_form_field.clean(url)
+    except ValidationError:
+        return False
+    return True
+
 while (True):
-    inp = input('Enter a sentence: ')
+    inp = input('Enter a link: ')
+
 
     if inp == 'q':
         exit()
 
-    print(factcheck.factAnalysis(inp))
+
+    if validate_url(inp):
+        try:
+            article = Article(url=inp.strip(' '), language='en')
+            article.download()
+            article.parse()
+            print(article.text)
+            print(factcheck.factAnalysis(article.text))
+        except Exception:
+            article = ''
+            r = requests.get(inp)
+            soup = BeautifulSoup(r.text, "html.parser")
+            for this_div in soup.find_all("p"):
+                article += this_div.get_text()
+            print(article)
+            if not article:
+                print('Invalid URL')
+            else:
+                print(factcheck.factAnalysis(article))
+    else:
+        print(factcheck.factAnalysis(inp))
+
+
--- factcheck/factcheck.py
+++ factcheck/factcheck.py
@@ -3,7 +3,7 @@
 '''
 
 
-from nltk import word_tokenize
+from nltk import word_tokenize, sent_tokenize
 import pickle
 from nltk.classify import ClassifierI
 from statistics import mode
@@ -23,16 +23,6 @@ class VoteClassifier(ClassifierI):
         return mode(votes)
 
 
-    def confidence(self, features):
-        votes = []
-        for c in self._classifiers:
-            v = c.classify(features)
-            votes.append(v)
-
-        choiceVotes = votes.count(mode(votes))
-        conf = choiceVotes/len(votes)
-
-        return conf
 
 
 documentsFile = open('saved_documents.pickle', 'rb')
@@ -90,5 +80,19 @@ voteClassifier = VoteClassifier(MultinomialNBClassifier,
                                 LinearSVCClassifier)
 
 def factAnalysis(text):
-    features = findFeatures(text)
-    return voteClassifier.classify(features), voteClassifier.confidence(features)
+    text = sent_tokenize(text)
+    trueCount = 0.00
+    falseCount = 0.00
+    sentenceClass = []
+    for sentence in text:
+        features = findFeatures(sentence)
+        if voteClassifier.classify(features):
+            trueCount += 1
+            sentenceClass.append(True)
+        else:
+            falseCount += 1
+            sentenceClass.append(False)
+    if trueCount > falseCount:
+        return True, 1.00 - falseCount/trueCount
+    else:
+        return False, 1.00 - trueCount/falseCount
